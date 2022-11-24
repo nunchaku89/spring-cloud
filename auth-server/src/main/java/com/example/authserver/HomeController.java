73fd36a0d60fde0a851b52f61d6e87d173fa8264
nunchaku89 <nunchaku89@penta.co.kr>
2022-11-24 14:38:46 +0900

    JWT 인증

--- auth-server/src/main/java/com/example/authserver/HomeController.java
+++ auth-server/src/main/java/com/example/authserver/HomeController.java
@@ -1,28 +1,17 @@
 package com.example.authserver;
 
-import com.example.authserver.model.JwtRequest;
-import com.example.authserver.model.JwtResponse;
 import com.example.authserver.model.User;
-import com.example.authserver.properties.JwtProperties;
-import com.example.authserver.service.CustomUserDetails;
 import com.example.authserver.service.CustomUserDetailsService;
 import com.example.authserver.util.JwtUtil;
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.Date;
-import java.util.HashMap;
-import java.util.Map;
 
 @RestController
 public class HomeController {
